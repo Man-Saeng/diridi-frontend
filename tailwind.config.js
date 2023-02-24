9d842f5a14b06c62df2abae3a7fe70f731ebff6c
dkile <dkile532@naver.com>
2023-02-24 16:24:43 +0900

    Add px to rem theme in tailwilnd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
+
+const range = (s = 0, e) => Array.from({ length: e - s }, (_, i) => s + i);
+
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        ...range(1, 100).reduce((acc, px) => {
+          acc[`${px}pxr`] = pxToRem(px);
+          return acc;
+        }, {}),
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/line-clamp"),
